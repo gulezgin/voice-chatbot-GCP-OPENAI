b9c717502b15676951645d4e1c084d4020fec8d1
Tarık Gülezgin <81231546+gulezgin@users.noreply.github.com>
2024-09-11 17:06:45 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,18 +3,15 @@ from gpt_integration import gpt_ile_cevapla
 from text_to_speech import metni_sese_cevir
 
 def sesli_chatbot():
-    # Ses kaydet
+  
     kayit_dinle()
 
-    # Sesi metne çevir
     kullanici_metin = ses_to_metn()
     print("Kullanıcı Metni:", kullanici_metin)
 
-    # GPT-4 ile cevap üret
     chatbot_cevabi = gpt_ile_cevapla(kullanici_metin)
     print("Chatbot Cevabı:", chatbot_cevabi)
-
-    # Cevabı sese çevir
+    
     metni_sese_cevir(chatbot_cevabi)
 
 if __name__ == "__main__":
